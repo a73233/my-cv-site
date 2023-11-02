ef9fc8a72f1a5c7079fa14c06b70706820665427
a73233 <luisneto95@gmail.com>
2023-11-02 21:31:29 +0000

    svelte config mod

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
       fallback: "404.html",
     }),
     paths: {
-      base: dev ? "" : process.env.BASE_PATH,
+      base: process.env.NODE_ENV === "production" ? "/my-cv-site" : "",
     },
   },
   preprocess: vitePreprocess(),
