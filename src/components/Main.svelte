ac1781880505f7b3183dc573d53314d985957f2e
Luis Neto <luisneto95@gmail.com>
2025-01-22 15:44:24 +0000

    Revert "added a few links and a small animation"
    This reverts commit cdb3f2196686e8875fe4c0b8ebd4f1b94ea84e90.

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -103,9 +103,9 @@
     </div>
     <div class="relative shadow-2xl grid place-items-center">
       <img
-      src={"images/profile.png"}
-      alt="Zetane Engine"
-      class="object-cover z-[2] max-h-[60vh] transition-transform duration-300 transform hover:scale-110 hover:-rotate-6"
+        src={"images/profile.png"}
+        alt="Zetane Engine"
+        class="object-cover z-[2] max-h-[60vh]"
       />
     </div>
     <!-- <div  class="flex p-0.5 relative max-w-[700px] w-full mx-auto">
@@ -193,12 +193,6 @@
               >neural networks</strong
             > back into the running process for visible action and evaluation.
           </li>
-            <li>
-              Click <a href="https://repositorium.sdum.uminho.pt/handle/1822/92593" target="_blank" class="text-violet-400">here</a> for the RepositoriUM.
-            </li>
-            <li>
-              Verify <a href="http://cp.eng.uminho.pt/prova.aspx?id=7464" target="_blank" class="text-violet-400">here</a> the evaluation.
-            </li>
         </ul>
       </Step>
     </div>
