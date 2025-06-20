9da00abf1e2e816a56b02004aab464a45370b570
a73233 <luisneto95@gmail.com>
2025-06-20 23:36:50 +0100

    changed favicons logic

--- docs/_app/immutable/chunks/paths.129d1fb2.js
+++
@@ -1,1 +0,0 @@
-var s;const l=((s=globalThis.__sveltekit_lln11x)==null?void 0:s.base)??"/my-cv-site";var e;const t=((e=globalThis.__sveltekit_lln11x)==null?void 0:e.assets)??l;export{t as a,l as b};
