4a57a573f50bc6792424e262929349475e92abba
a73233 <luisneto95@gmail.com>
2025-06-20 17:06:53 +0100

    added more favicons, og cards,, ld json

---
+++ docs/_app/immutable/chunks/paths.129d1fb2.js
@@ -0,0 +1,1 @@
+var s;const l=((s=globalThis.__sveltekit_lln11x)==null?void 0:s.base)??"/my-cv-site";var e;const t=((e=globalThis.__sveltekit_lln11x)==null?void 0:e.assets)??l;export{t as a,l as b};
